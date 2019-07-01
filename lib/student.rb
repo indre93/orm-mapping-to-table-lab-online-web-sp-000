5eb45f05b1600260fbe228033a6ac65840cc9546
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 20:06:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,7 @@ class Student
       VALUES (?,?)
    SQL
    DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_row_id() FROM songs")[0][0]
  end
 
  def self.create

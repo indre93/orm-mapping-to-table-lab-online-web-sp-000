e45a39de1a8080fce610e787d778f33d1c2f46d0
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 20:03:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,11 +30,11 @@ class Student
  end
 
  def save
-   sql = <<-SQL
+   sql = <<-SQL 
       INSERT INTO students (name, grade)
       VALUES (?,?)
    SQL
-   DB[:conn].execute(sql)
+   DB[:conn].execute(sql, self.name, self.grade)
  end
 
  def self.create

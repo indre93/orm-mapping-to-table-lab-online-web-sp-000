8520d79066e7fdf2bb5ae1c29a6cb6519d886f05
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 19:47:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,19 @@ class Student
     @id = id
   end
 
+  def self.create_table
+   sql =  <<-SQL 
+     CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY, 
+       name TEXT, 
+       grade TEXT
+       )
+       SQL
+   DB[:conn].execute(sql) 
+ end
+
+  def self.create_table
+    
 
 
 end
